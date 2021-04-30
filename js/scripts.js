0c94d2b612e497d3d1043f3bbeb6ddda23715efe
giovannifoletto <67295809+giovannifoletto@users.noreply.github.com>
2021-04-30 23:59:37 +0200

    Cambiato tutta la home page, sto modificando ItalyMap affinché riesca a venire con un opacità maggiore quando ci si trova sopra

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function ReloadSlideMenu(){
+    var doc = document.getElementById("mobile-menu").hidden;
+    if ( doc == false)
+        return document.getElementById("mobile-menu")
+}
+
+function changeColor(){
+    return ;
+}
