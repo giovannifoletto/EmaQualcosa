c5fb83156eb2c2893baa4865b73a2e3464832875
giovannifoletto <giovanni.foletto@gmail.com>
2021-09-09 11:06:30 +0200

    Forse funziona

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,261 @@
-function ReloadSlideMenu(){
-    var doc = document.getElementById("mobile-menu").hidden;
-    if ( doc == false)
-        return document.getElementById("mobile-menu")
-}
+// console.log("External file executed");
 
-function changeColor(obj){
-    return;
-}
+$(document).ready( () =>{
+    console.log("Document ready");
+});
 
-function onLoad(){
-    console.log("Ciao");
-}
+function setOpacityValues(){
+    $('#Trento').hover( function() {$('#Trento').css('opacity', '50%')}, 
+        function(){ $('#Trento').css("opacity", "100%") } 
+    );
+    $('#Rimini').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#Rimini-ForliCesena').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#Ravenna').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#Ferrara').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#Bologna').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#Modena').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#ReggioEmilia').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#Parma').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    // QUI
+    $('#Piacenza').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#Pavia').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#Milano-Lodi').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#Cremona-Mantova').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#Brescia').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#Bergamo').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#Como-Lecco-MonzaBrianza').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#Varese').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#Sondrio').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#Bolzano').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#Verona').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#Vicenza').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#Padova').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#Rovigo').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#Treviso').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#Belluno').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#Pordenone').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#Udine').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#Venezia2').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#Venezia1').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#Venezia3').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#Gorizia').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    $('#Gorizia2').hover( 
+        function() {
+            $('#Trento').css('opacity', '50%')
+        }, 
+        function(){ 
+            $('#Trento').css("opacity", "100%")
+        } 
+    );
+    
+};
